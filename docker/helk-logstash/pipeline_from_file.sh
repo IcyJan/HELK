03287f292168059b6bbd6d1667b5563303d4f874
jurech <jurech@student.ethz.ch>
2022-03-22 08:47:59 +0100

    adding pipelines to record and replay data

---
+++ docker/helk-logstash/pipeline_from_file.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cp pipeline/0003-attack-input.conf pipeline/pipe_from_file
+cp pipeline/*filter.conf pipeline/pipe_from_file
+cp pipeline/*output.conf pipeline/pipe_from_file
---
+++ docker/helk-logstash/pipeline_to_file.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cp pipeline/*input.conf pipeline/pipe_to_file
